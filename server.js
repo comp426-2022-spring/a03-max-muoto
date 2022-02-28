53595daa54e8b69a6446df0bee97b88c7f2d181e
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-02-28 09:26:28 -0500

    First Steps

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const minimist = require('minimist');
+
+const app = express()
+const argv = (minimist)(process.argv.slice(2));
+
+argv["port"];
+const HTTP_PORT = argv.port || 5000;
+
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
+
+
+// Define Check Endpoints
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
